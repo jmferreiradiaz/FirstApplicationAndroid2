da4e935a0df05ff0c58db38cc0ca18b223453576
Abraham López <abraham.lopez@iesnervion.es>
2022-10-10 10:50:32 +0200

    Hecho align y parte de gallery

--- app/src/main/java/com/example/firstapplication/ui/align/AlignTextFragment.kt
+++ app/src/main/java/com/example/firstapplication/ui/align/AlignTextFragment.kt
@@ -2,16 +2,23 @@ package com.example.firstapplication.ui.align
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.firstapplication.R
 
 class AlignTextFragment : Fragment() {
 
     companion object {
         fun newInstance() = AlignTextFragment()
+        private const val TAG = "AlignTextFragment"
     }
 
     private lateinit var viewModel: AlignTextViewModel
@@ -20,13 +27,40 @@ class AlignTextFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_aligntext, container, false)
+        val view = inflater.inflate(R.layout.fragment_aligntext, container, false)
+
+        val inputText = view.findViewById<EditText>(R.id.inputTextToAlign)
+        val text = view.findViewById<TextView>(R.id.textToAlign)
+
+        val btnLeft = view.findViewById<Button>(R.id.btnLeft)
+        val btnRight = view.findViewById<Button>(R.id.btnRight)
+        btnLeft.setOnClickListener { onClickAlignChange(text, btnLeft, inputText) }
+        btnRight.setOnClickListener { onClickAlignChange(text, btnRight, inputText) }
+
+
+        inputText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) { }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                text.text = s
+            }
+        })
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(AlignTextViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun onClickAlignChange(text: TextView, btn: Button, input: EditText) {
+        if(btn.id == R.id.btnLeft) text.textAlignment = 2
+        else if(btn.id == R.id.btnRight) text.textAlignment = 3
+        text.text = input.text
     }
 
 }
--- app/src/main/java/com/example/firstapplication/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/firstapplication/ui/gallery/GalleryFragment.kt
@@ -1,15 +1,24 @@
 package com.example.firstapplication.ui.gallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.helper.widget.Carousel
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.firstapplication.R
 import com.example.firstapplication.databinding.FragmentGalleryBinding
+import com.example.firstapplication.ui.changecolor.ChangeColorFragment
 
 class GalleryFragment : Fragment() {
+
+    companion object {
+        private const val TAG = "GalleryFragment"
+    }
 
     private var _binding: FragmentGalleryBinding? = null
 
@@ -22,21 +31,35 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val view = inflater.inflate(R.layout.fragment_gallery, container, false)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val carousel = view.findViewById<Carousel>(R.id.carousel)
+        val btnPrevious = view.findViewById<Button>(R.id.btnPrevious)
+        val btnNext = view.findViewById<Button>(R.id.btnNext)
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        btnPrevious.setOnClickListener { onClickChangeImage(btnPrevious, view, carousel) }
+        btnNext.setOnClickListener { onClickChangeImage(btnNext, view, carousel) }
+
+        carousel.setAdapter(object : Carousel.Adapter {
+            override fun count(): Int {
+                return 5
+            }
+
+            override fun populate(view: View, index: Int) {
+                // need to implement this to populate the view at the given index
+            }
+
+            override fun onNewItem(index: Int) {
+                // called when an item is set
+            }
+        })
+
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun onClickChangeImage(btn: Button, view: View, carousel: Carousel) {
+        Log.d(TAG, "Entra")
+        if(btn.id == R.id.btnNext) carousel
+        else if(btn.id == R.id.btnPrevious) carousel.nextFocusDownId
     }
 }
